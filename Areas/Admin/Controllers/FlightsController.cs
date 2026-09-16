4fc0e72251c3580183c78d5d0269e939084bf9ac
Mert <mertsenel6223@gmail.com>
2026-09-16 16:44:14 +0300

    Admin Paneli eklendi

---
+++ Areas/Admin/Controllers/FlightsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightBooking.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class FlightsController : Controller
+    {
+        public IActionResult FlightList()
+        {
+            return View();
+        }
+    }
+}
